df3488b9d4be7f5293b3eaf85386c93043603a8a
Spreco <dinospreco@gmail.com>
2018-07-17 18:46:15 +0200

    Add AcessFlagService

--- project/src/main/java/com/bakery/services/AccessFlagService.java
+++ project/src/main/java/com/bakery/services/AccessFlagService.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public interface AccessFlagService {
 
-    List<AccessFlag> getAllAccesFlags() throws ServiceException;
-    List<AccessFlag> getAccesFlagsByRole(String role) throws ServiceException;
+    List<AccessFlag> getAllAccessFlags() throws ServiceException;
+    List<AccessFlag> getAccessFlagsByRole(String role) throws ServiceException;
     AccessFlag getAccessFlagByAccessFlag(String accessFlagg) throws ServiceException;
     AccessFlag getAccessFlagById(Long id) throws ServiceException;
     AccessFlag saveAccessFlag(AccessFlag accessFlag) throws ServiceException;
-    AccessFlag updateAccesFlag(AccessFlag accessFlag) throws ServiceException;
-
+    AccessFlag updateAccessFlag(AccessFlag accessFlag) throws ServiceException;
+    void deleteAccessFlag(AccessFlag accessFlag) throws ServiceException;
 }
